c7a85f9d113a200273d3b1d6b4f79e80321f0d4a
gicherui <gideon.cheruiyot@microsoft.com>
2020-08-05 14:18:56 -0700

    Add project files.

---
+++ AuthCookie/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AuthCookie.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AuthCookie.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Users()
+        {
+            var uses = new Users();
+            return View(uses.GetUsers());
+        }
+
+    }
+}
+
+  
